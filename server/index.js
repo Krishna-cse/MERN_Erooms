faf265aaa9a9e7768d05cae31ab64850b83fce86
Krishna-cse <krishnaofficialemail@gmail.com>
2024-01-21 18:45:41 +0530

    create signup api route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -17,9 +18,11 @@ mongoose.connect(`mongodb+srv://itskrishnacse:${encodedPassword}@mern-erooms.iek
    });
 
 const app = express();
+app.use(express.json())
 
 //middleware
 app.use("/api/user",userRouter)
+app.use("/api/auth", authRouter )
 
 
 

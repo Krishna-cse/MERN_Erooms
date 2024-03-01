279466b607867b02bce20a70530a5041bbf3d97f
Krishna-cse <krishnaofficialemail@gmail.com>
2024-03-01 19:47:48 +0530

    Complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,8 @@ export default function Profile() {
   const [filePerc, setFilePerc] = useState(0)
   const [formData,setFormData]=useState({})
   const [updateSuccess,setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings,setUserListings] = useState([])
   const dispatch = useDispatch();
   
 
@@ -111,6 +113,23 @@ export default function Profile() {
     }
   }
 
+  const handleShowListings = async() =>{
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`)
+      const data = await res.json();
+      if(data.success === false){
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data)
+    } catch (error) {
+      setShowListingsError(true);
+      
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto ' >
      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1> 
@@ -153,6 +172,31 @@ export default function Profile() {
      <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p>
+      <button onClick={handleShowListings} className='text-blue-700 w-full'> Showing Listings</button>
+      <p className='text-red-700 mt-5'>{showListingsError ? 'Error Showing Listings':''}</p>
+      {userListings && userListings.length > 0 && 
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+      
+      {userListings.map((listing) =>(
+        <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+          <Link to={`/listing/${listing._id}`}>
+            <img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain rounded-lg'/>
+          </Link>
+          <Link className='text-slate-700 font-semibold hover:underline truncate flex-1' to={`/listing/${listing._id}`}>
+            <p>{listing.name}</p>
+
+          </Link>
+
+          <div className='flex flex-col item-center'>
+            <button className='text-red-700 uppercase'>Delete</button>
+            <button className='text-blue-700 uppercase'>Edite</button>
+
+          </div>
+
+        </div>
+      ) )}
+      </div>}
     </div>
   )
 }     
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -9,5 +9,4 @@ router.post("/update/:id",verifyToken, updateUser)
 router.delete("/delete/:id",verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
 
-
 export default router;
